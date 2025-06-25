df4bb9a7f3cca1bb0c8c65b10ae09c00ebdf486c
Nsfr750 <nsfr750@gmail.com>
2025-06-25 14:29:34 +0200

    0.0.3

--- setup.py
+++ setup.py
@@ -1,34 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="comicdb",
+    name="ComicDB",
     version="0.3.0",
     packages=find_packages(),
-    install_requires=[
-        # Aggiungi qui le dipendenze del tuo progetto
-        'pillow',
-        'comicapi',
-        'rarfile',
-        'pikepdf',
-        'python-magic',
-        'ttkbootstrap',
-    ],
-    # Metadati aggiuntivi
-    author="Nsfr750",
-    author_email="nsfr750@yandex.com",
-    description="Un'applicazione per la gestione e la visualizzazione di fumetti digitali",
-    long_description=open('README.md', encoding='utf-8').read(),
+    install_requires=[],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Nsfr750/ComicDB",
+    url="https://github.com/yourusername/ComicDB",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL3 License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
-    entry_points={
-        'console_scripts': [
-            'comicdb=comicdb.main:main',
-        ],
-    },
+    python_requires='>=3.6',
 )
