b89e1f0c420fe0efea3577f629d6de93e4fd7d1a
Nsfr750 <nsfr750@gmail.com>
2025-06-25 10:06:53 +0200

    0.0.3

--- comp.bat
+++ comp.bat
@@ -1,6 +1,21 @@
-@ECHO OFF
+@ECHO ON
 @x:
 @cd "x:/GitHub/ComicDB"
 @ECHO
-@nuitka --onefile --output-dir=x:/GitHub/ComicDB --output-filename=ComicDB.exe --windows-icon-from-ico=x:/GitHub/ComicDB/images/icon.ico --plugin-enable=tk-inter --follow-imports --show-progress --windows-dependency-tool=upx --windows-company-name=Tuxxle --windows-product-name=ComicDB --windows-product-version=1.0.0 --windows-file-version=1.0.0 x:/GitHub/ComicDB/main.py
+@python -m nuitka ^ 
+--onefile ^
+--output-dir=X:/GitHub/ComicDB ^
+--output-filename=ComicDB.exe ^
+--windows-icon-from-ico=X:/GitHub/ComicDB/images/icon.ico ^
+--plugin-enable=tk-inter ^
+--follow-imports ^
+--show-progress ^
+--assume-yes-for-downloads ^
+--windows-console-mode=disable ^
+--windows-company-name=Tuxxle ^
+--windows-product-name=ComicDB ^
+--windows-product-version=0.0.3 ^
+--windows-file-version=0.0.3 ^
+main.py
 @pause
+
